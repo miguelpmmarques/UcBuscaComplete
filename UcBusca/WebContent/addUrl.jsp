48eb9de754d1a3fc944dd8e1c69574cbac42ca91
miguelpmmarques <miguelpmmarques@gmail.com>
2019-12-05 20:38:11 +0000

    Carregamento

---
+++ UcBusca/WebContent/addUrl.jsp
@@ -0,0 +1,58 @@
+<%@ taglib prefix="s" uri="/struts-tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+		 pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css" >
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Uc Busca</title>
+</head>
+<body>
+<s:include value="navbar.jsp"></s:include>
+
+<div class="col-12 text-center">
+
+		<img class="logo" src="logo-SD.png">
+	</div>
+<div class="col-12 text-center">
+		<c:choose>
+		<c:when test="${session.loggedin == true}">
+
+
+			<h1> <b> ${session.username}, Uc Busca? </b> </h1>
+			</c:when>
+			<c:otherwise>
+				<h1> <b><s:text name="Uc Busca? " /></b> </h1>
+			</c:otherwise>
+
+			</c:choose>
+		<br>
+		<s:form action="search" method="post">
+			<s:textfield cssClass = "searchlanding" name="SearchModel.seachWords" />
+			<s:submit  cssClass="btn btn-light btn-outline-secondary smallmarginleft" value="Search" />
+		</s:form>
+
+	</div>
+
+<footer class="page-footer font-small fixed-bottom">
+
+	<!-- Copyright -->
+	<div class="footer-copyright text-center py-3">
+		Find referenced URLS in the search bar! Start with http:// or https://
+	</div>
+	<div class="footer-copyright text-center py-3 bg-warning">
+		© 2019 Copyright: Miguel Marques & Paulo Cardoso
+	</div>
+	<!-- Copyright -->
+
+</footer>
+
+</body>
+
+</html>
---
+++ UcBusca/src/ucbusca/model/SystemInfoModel.java
@@ -0,0 +1,110 @@
+package ucbusca.model;
+
+import RMISERVER.ServerLibrary;
+import RMISERVER.User;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+
+public class HistoryModel {
+	private ServerLibrary ucBusca;
+	private Properties prop = new Properties();
+	private Map<String, Object> session;
+
+	public HistoryModel(Map<String, Object> session) {
+		this.session = session;
+		String propFileName = "RMISERVER/config.properties";
+		InputStream inputStream = HistoryModel.class.getClassLoader().getResourceAsStream(propFileName);
+		try {
+			this.prop.load(inputStream);
+		} catch (Exception e){
+			System.out.println(e);
+			System.out.println("Cannot read properties File");
+			return;
+		}
+		System.out.println(prop.getProperty("REGISTRYIP"));
+		System.out.println(prop.getProperty("REGISTRYPORT"));
+		System.out.println(prop.getProperty("LOOKUP"));
+
+
+			try {
+				this.ucBusca = (ServerLibrary) LocateRegistry.getRegistry(prop.getProperty("REGISTRYIP"), Integer.parseInt(prop.getProperty("REGISTRYPORT") )).lookup(prop.getProperty("LOOKUP") );
+
+				//this.ucBusca = (ServerLibrary) Naming.lookup(prop.getProperty("LOOKUP"));
+
+				System.out.println("Connected to UcBusca");
+
+			} catch (Exception e) {
+				System.out.println(e);
+				System.out.println("Connecting...");
+				try {
+					Thread.sleep(2000);
+				}catch (InterruptedException es){
+					System.out.println("Sleep interrupted");
+				}
+			}
+
+	}
+
+	
+	public ArrayList<String> getHistory() throws InterruptedException, RemoteException, NotBoundException {
+		HashMap<String,String> protocol;
+		System.out.println("CHEGOU AQUI");
+		ArrayList<String> anwser = new ArrayList<>();
+
+
+		User thisUser = (User)session.get("user");
+		System.out.println("CHEGOU AQUI");
+
+		protocol = retry(thisUser,0);
+		System.out.println("CHEGOU AQUI-----"+protocol);
+		System.out.println(protocol);
+		int arraySize = Integer.parseInt((String)protocol.get("word_count"));
+
+		System.out.println(arraySize);
+		if (arraySize == 0)
+			anwser.add("Empty");
+		for(int i = arraySize ;i>0;i--){
+			System.out.println((String)protocol.get("word_"+i));
+			anwser.add((String)protocol.get("word_"+i));
+		}
+		return anwser;
+	}
+
+
+
+	private HashMap<String,String> retry(Object parameter,int replyCounter) throws RemoteException, InterruptedException, NotBoundException {
+		HashMap<String,String> myDic;
+		try {
+			this.ucBusca=(ServerLibrary) LocateRegistry.getRegistry(prop.getProperty("REGISTRYIP"), Integer.parseInt(prop.getProperty("REGISTRYPORT"))).lookup(prop.getProperty("LOOKUP"));
+			myDic = this.ucBusca.getHistory((User)parameter);
+			System.out.println(myDic);
+			return myDic;
+
+		}catch (Exception e) {
+			try {
+				Thread.sleep(2000);
+			} catch(InterruptedException e2) {
+				System.out.println("Interrupted");
+			}
+			if (replyCounter>16){
+				System.out.println("Please, try no reconnect to the UcBusca");
+				System.exit(0);
+			}
+			System.out.println(e);
+			System.out.println("Retransmiting... "+replyCounter);
+			retry(parameter,++replyCounter);
+		}
+		return new HashMap<String,String>();
+	}
+}
